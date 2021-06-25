ad3e9e4e254d85aced4b05cb014c7d9b8deae6ae
haenim <dlgoslaek1@gmail.com>
2021-06-25 13:32:59 +0900

    add download, upload, delete function

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Resource, Api
 from auth import Auth
 import json
-
-
+import boto3
+import config
+from pathlib import Path
+from botocore.exceptions import ClientError
+import logging
+import jsonify
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -30,6 +34,36 @@ api = Api(
 
 api.add_namespace(Auth, '/auth')
 """
+s3 = boto3.client('s3',
+                    aws_access_key_id=config.ACCESS_KEY_ID,
+                    aws_secret_access_key= config.ACCESS_SECRET_KEY,
+                    aws_session_token= config.AWS_SESSION_TOKEN
+                     )
+
+BUCKET_NAME = config.BUCKET_NAME
+
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    try:
+        response = s3.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
 
 class User(db.Model):
     """ Create user table"""
@@ -50,7 +84,7 @@ def home():
     else:
         if request.method == 'POST':
             return render_template('index.html')
-        return render_template('index.html')
+        return render_template("file_upload_to_s3.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -64,8 +98,11 @@ def login():
         try:
             data = User.query.filter_by(user_id=id, password=passw).first()
             if data is not None:
+                print("login")
                 session['logged_in'] = True
-                return redirect(url_for('home'))
+                session['id'] = id
+                return render_template("file_upload_to_s3.html")
+
             else:
                 return render_template("auth-sign-in.html", data = "아이디나 비밀번호가 일치하지 않습니다.")
         except:
@@ -123,6 +160,81 @@ def logout():
     """Logout Form"""
     session['logged_in'] = False
     return redirect(url_for('home'))
+
+
+
+
+
+
+## s3
+
+@app.route('/upload',methods=['post'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        id = session.get('id')
+        if file:
+                filename = file.filename
+                s3.put_object(
+                    Bucket = BUCKET_NAME,
+                    Body= file,
+                    Key = id+'/'+filename
+                )
+                msg = "Upload Done ! "
+
+    return render_template("file_upload_to_s3.html",msg =filename)
+
+
+@app.route('/file', methods=['GET'])
+def download_file():
+    id = session.get('id')
+
+    filename = request.args.get("filename")
+    file_path = id +'/' + filename
+    downloads_path = str(Path.home() / "Downloads")
+    ## window User/downloads
+    s3.download_file(BUCKET_NAME, file_path,downloads_path+'\\'+filename)
+
+    msg_down = "Download Done ! "
+
+    return render_template("file_upload_to_s3.html",msg_down = msg_down)
+
+
+@app.route('/url', methods=['GET'])
+def download_url():
+    id = session.get('id')
+    filename = request.args.get("filename")
+
+    file_path = id +'/' + filename
+    url =create_presigned_url(BUCKET_NAME,file_path)
+
+    msg_url = "Url Done ! "
+
+    return render_template("file_upload_to_s3.html",msg_url = url) 
+
+
+@app.route('/delete', methods=['GET'])
+def delete_file():
+    id = session.get('id')
+    filename = request.args.get("filename")
+
+    file_path = id +'/' + filename
+    print(file_path)
+    response = s3.delete_object(Bucket=BUCKET_NAME, Key=file_path)
+
+    return render_template("file_upload_to_s3.html",msg_delete = response) 
+
+
+
+@app.route('/list', methods=['GET'])
+def list():
+    id = session.get('id')
+    path = request.args.get("path")
+
+    path = id +'/' + path
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter = path)
+    return str(response["Contents"])
+
 
 
 if __name__ == '__main__':
